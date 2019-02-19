117a5770454787783c970dee42b8c6177b73a012
mmoiyadi-rlzn <mmoiyadi@realization.com>
2019-02-19 16:03:33 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.set('views', './src/views');
+//app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
+
+app.get("/", function (req, res) {
+    //res.sendFile(path.join(__dirname, 'views/index.html'));
+    res.render('index', { list: ['a','b'], title: 'My Movies'});
+});
+
+app.listen(port, function () {
+    debug(`listening at port ${chalk.green(port)}`);
+});
