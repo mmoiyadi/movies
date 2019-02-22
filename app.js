cf7b4ed0c3581992efd6d529ada8f725e1817bd4
mmoiyadi <mohammed.moiyadi@gmail.com>
2019-02-22 23:57:01 +0530

    Setting up SQL server to retrieve movies data

--- app.js
+++ app.js
@@ -3,10 +3,23 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const sql = require('mssql');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const config = {
+    user: 'mmoiyadi',
+    password: 'Real$123',
+    server: 'mmmovies.database.windows.net', // You can use 'localhost\\instance' to connect to named instance
+    database: 'MMMovies',
+ 
+    options: {
+        encrypt: true // Use this if you're on Windows Azure
+    }
+}
+
+sql.connect(config).catch(err => debug(err));
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
@@ -16,9 +29,23 @@ app.set('views', './src/views');
 //app.set('view engine', 'pug');
 app.set('view engine', 'ejs');
 
+const nav =  [
+    {title: 'Movies', link:'/movies'},
+    {title: 'Directors', link: '/directors'}
+]; 
+
+const movieRouter = require('./src/routes/movieRoute')(nav);
+ 
+app.use('/movies',movieRouter);
+
 app.get("/", function (req, res) {
     //res.sendFile(path.join(__dirname, 'views/index.html'));
-    res.render('index', { list: ['a','b'], title: 'My Movies'});
+    res.render(
+        'index', 
+        { 
+            nav: [{title: 'Movies',link:'/movies'},
+                  {title: 'Directors', link: '/directors'}], 
+            title: 'My Movies'});
 });
 
 app.listen(port, function () {
